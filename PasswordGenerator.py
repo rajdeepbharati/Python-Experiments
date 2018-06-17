dd62e332d52bc748db3f4ceacabf066c5f21b6df
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-06-17 19:54:51 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,14 +2,14 @@ import random
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*_-+=`|\(){}[]:;"\'<>,.?/'
 
-n = input('number of passwords?')
+n = input('number of passwords? ')
 n = int(n)
 
-length = input('password length?')
+length = input('password length? ')
 length = int(length)
 
 for p in range(n):
-  password = ''
-  for c in range(length):
-    password += random.choice(chars)
-  print(password)
+	password = ''
+	for c in range(length):
+		password += random.choice(chars)
+	print(password)
